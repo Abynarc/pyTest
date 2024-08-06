dc81021c5f1c6574197a058dc81c55d76ebbba97
Ilia Baranov <clod17@yandex.ru>
2024-08-06 19:48:13 +0300

    a

---
+++ pythonProject/tests/test_mail.py
@@ -0,0 +1,10 @@
+import pytest
+@pytest.fixture()
+
+def set_up():
+    print('Авторизация выполнена')
+def send_mail_1():
+    print('Письмо отправлено')
+
+def send_mail_2():
+    print('Второе письмо отправлено')
